119c74480dcbaa2b10a8eb32f1d253e99e9240e0
srvshrm <40930291+srvshrm@users.noreply.github.com>
2018-08-27 17:37:19 +0530

    Final

--- server/modules/newsfeed/posts.js
+++ server/modules/newsfeed/posts.js
@@ -1,39 +1,56 @@
-/*
-    @author: Sourav Sharma, Surbhi Kulkarni, Richa Madhupriya
- */
-
 const Dao = require('../data-access/data-access');
 const dao = new Dao();
 
+//required to generate unique Id
 const uuidv4 = require('uuid/v4');
 
 
 class Posts {
 
-//to insert post in database by a user
-    async createPosts(data){
+/**
+ * @description to insert post in the database by the user
+ * @author Sourav Sharma, Surabhi Kulkarni, Richa Madhupriya
+ * @param {Object} data
+ * @param {string} userName
+ * @returns {Object} result 
+ */
+    async createPosts(data,userName){
         data.postid = uuidv4();
-        var filter = { "userId": 1005 };
+        var filter = { "userName": userName};
         var content = { $push: { "posts": data } };
-        var result = await dao.update("test",filter,content)
+        var result = await dao.update("users",filter,content)
         return result
     }
 
-//to edit post inserted in the database by the user
-    async editPosts(data){
-        var filter = { "posts.postid": 1 };
+/**
+ * @description to edit post inserted in the database by the user
+ * @author Sourav Sharma, Surabhi Kulkarni, Richa Madhupriya
+ * @param {Object} data
+ * @param {string} userName
+ * @param {string} postId
+ * @returns {Object} result 
+ */
+    async editPosts(data,userName,postId){
+        var filter = {"userName":userName, "posts.postid": postId };
         var content = { $set: { "posts.$.content": data.content } };
-        var result = await dao.update("test",filter,content)
+        var result = await dao.update("users",filter,content)
         return result
     }
 
-//to delete post inserted in the database by the user
-     async deletePosts(){
-        var filter = { "userId": 1005 };
-        var content = { $pull: { "posts": {postid:1} } };
-        var result = await dao.update("test",filter,content)
+/**
+ * @description to delete post inserted in the database by the user
+ * @author Sourav Sharma, Surabhi Kulkarni, Richa Madhupriya
+ * @param {string} userName
+ * @param {string} postId
+ * @returns {Object} result 
+ */
+     async deletePosts(userName,postId){
+        var filter = { "userName": userName};
+        var content = { $pull: { "posts": {postid: postId} } };
+        var result = await dao.update("users",filter,content)
         return result
     }
+
 }
 
 module.exports = Posts
