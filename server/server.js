ec2dc7c295264b06905666ce420f257e53d02ef9
srvshrm <40930291+srvshrm@users.noreply.github.com>
2018-08-27 17:43:30 +0530

    Final

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-/*
-    @author: Sourav Sharma, Surbhi Kulkarni, Richa Madhupriya
- */
+/**
+* @author: Sourav Sharma, Surabhi Kulkarni, Richa Madhupriya
+*/
 
 const express = require('express');
 const app = express();
@@ -8,15 +8,21 @@ const app = express();
 const Posts = require('./modules/newsfeed/posts');
 const post = new Posts();
 
+const Search = require('./modules/newsfeed/search');
+const search = new Search();
+
 var parser = require('body-parser');
 
 app.use(parser.json());
 
-//to insert post in database by a user
-app.patch('/rest/api/users/createPosts/update/', async (req, res) => {
+/**
+ * @description to insert post in database by a user
+*/
+app.patch('/rest/api/users/createPosts/update/:userName', async (req, res) => {
     let result;
+    let userName = req.params.userName;
     try {
-        result = await post.createPosts(req.body);
+        result = await post.createPosts(req.body,userName);
     }
     catch (err) {
         result = { err: err }
@@ -24,11 +30,15 @@ app.patch('/rest/api/users/createPosts/update/', async (req, res) => {
     res.send(result)
 });
 
-//to edit post inserted in the database by the user
-app.patch('/rest/api/users/editPosts/update/', async (req, res) => {
-    let result
+/**
+ * @description to edit post inserted in the database by the user
+ */
+app.patch('/rest/api/users/editPosts/update/:userName/:postId', async (req, res) => {
+    let result;
+    let userName = req.params.userName;
+    let postId = req.params.postId;
     try {
-        result = await post.editPosts(req.body);
+        result = await post.editPosts(req.body,userName,postId);
     }
     catch (err) {
         result = { err: err }
@@ -36,11 +46,15 @@ app.patch('/rest/api/users/editPosts/update/', async (req, res) => {
     res.send(result)
 })
 
-//to delete post inserted in the database by the user
-app.patch('/rest/api/users/deletePosts/update/', async (req, res) => {
+/**
+ * @description to delete post inserted in the database by the user
+ */
+app.patch('/rest/api/users/deletePosts/update/:userName/:postId', async (req, res) => {
     let result
+    let userName = req.params.userName;
+    let postId = req.params.postId;
     try {
-        result = await post.deletePosts();
+        result = await post.deletePosts(userName,postId);
     }
     catch (err) {
         result = { err: err }
@@ -48,5 +62,19 @@ app.patch('/rest/api/users/deletePosts/update/', async (req, res) => {
     res.send(result)
 })
 
+/**
+ * @description to search people in the database by the user
+ */
+app.patch('/rest/api/users/searchPeople/:userName', async (req, res) => {
+    let result;
+    let userName = req.params.userName;
+    try {
+        result = await search.searchPeople(userName);
+    }
+    catch (err) {
+        result = { err: err }
+    }
+    res.send(result)
+})
 
 app.listen('8080', () => console.log('Listening on port 8080'))
