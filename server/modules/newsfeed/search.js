95cf44c7ddc4fd35b76bf4e4dd65574d2258e876
srvshrm <40930291+srvshrm@users.noreply.github.com>
2018-08-28 15:08:21 +0530

    Company search added

--- server/modules/newsfeed/search.js
+++ server/modules/newsfeed/search.js
@@ -1,18 +1,32 @@
 const Dao = require('../data-access/data-access');
 const dao = new Dao();
 
-class Search
-{
+class Search {
     /**
- * @description to search people in the database by the name
- * @author Sourav Sharma, Surabhi Kulkarni, Richa Madhupriya
- * @param {string} userName
- * @returns {Object} result 
- */
+     * @description to search people in the database by the name
+     * @author Sourav Sharma, Surabhi Kulkarni, Richa Madhupriya
+     * @param {string} query
+     * @returns {Object} result 
+     */
 
- async searchPeople( userName) {
-        var filter = {"userName":  userName }
-        let result = await dao.find("users", filter);
+    async searchPeople(query) {
+        console.log(query);
+        var filter = { $match: { "name": new RegExp(query, 'i') } }
+        var project = { $project: { "_id": 0, "userName": 1, "name": 1, "bio": 1 } }
+        let result = await dao.aggregate("users", [filter, project]);
+        return (result);
+    }
+
+    /**
+     * @description to search comapnies in the database by the name
+     * @author Sourav Sharma, Surabhi Kulkarni, Richa Madhupriya
+     * @param {string} query
+     * @returns {Object} result 
+     */
+    async searchCompanies(query) {
+        var filter = { $match: { "name": new RegExp(query, 'i') } }
+        var project = { $project: { "_id": 0, "companyID": 1, "name": 1, "areaOfWork": 1 } }
+        let result = await dao.aggregate("companies", [filter, project]);
         return (result);
     }
 
